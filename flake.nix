c689bea32a1e2f66cd7debd054227e05ce0e6601
seth <getchoo@tuta.io>
2024-08-09 23:47:36 -0400

    nix: fenix -> nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,14 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-    fenix = {
-      url = "github:nix-community/fenix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        rust-analyzer-src.follows = "";
-      };
-    };
-
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -22,7 +14,6 @@
     {
       self,
       nixpkgs,
-      fenix,
       treefmt-nix,
     }:
     let
@@ -81,37 +72,21 @@
         system:
         let
           pkgs = nixpkgsFor.${system};
-          packages = self.packages.${system};
+          packages' = self.packages.${system};
 
-          mkStaticWith = pkgs.callPackage ./nix/static.nix {
-            inherit (packages) nixpkgs-tracker-bot;
-            fenix = fenix.packages.${system};
-          };
-
-          containerWith =
-            nixpkgs-tracker-bot:
-            let
-              arch = nixpkgs-tracker-bot.stdenv.hostPlatform.ubootArch;
-            in
-            pkgs.dockerTools.buildLayeredImage {
-              name = "nixpkgs-tracker-bot";
-              tag = "latest-${arch}";
-              config.Cmd = [ (lib.getExe nixpkgs-tracker-bot) ];
-              architecture = arch;
-            };
+          staticWith = pkgs.callPackage ./nix/static.nix { };
+          containerize = pkgs.callPackage ./nix/containerize.nix { };
         in
         {
-          nixpkgs-tracker-bot = pkgs.callPackage ./nix/package.nix {
-            version = self.shortRev or self.dirtyShortRev or "unknown";
-          };
+          nixpkgs-tracker-bot = pkgs.callPackage ./nix/package.nix { };
 
-          default = packages.nixpkgs-tracker-bot;
+          default = packages'.nixpkgs-tracker-bot;
 
-          static-x86_64 = mkStaticWith { arch = "x86_64"; };
-          static-arm64 = mkStaticWith { arch = "aarch64"; };
+          static-x86_64 = staticWith { arch = "x86_64"; };
+          static-arm64 = staticWith { arch = "aarch64"; };
 
-          container-x86_64 = containerWith packages.static-x86_64;
-          container-arm64 = containerWith packages.static-arm64;
+          container-x86_64 = containerize packages'.static-x86_64;
+          container-arm64 = containerize packages'.static-arm64;
         }
       );
     };
--- nix/static.nix
+++ nix/static.nix
@@ -1,45 +1,16 @@
-{
-  lib,
-  fenix,
-  pkgsCross,
-  nixpkgs-tracker-bot,
-}:
+{ pkgsCross }:
 let
   crossPkgsFor = with pkgsCross; {
     x86_64 = musl64.pkgsStatic;
     aarch64 = aarch64-multiplatform;
   };
-
-  rustcTargetFor = lib.mapAttrs (lib.const (
-    pkgs: pkgs.stdenv.hostPlatform.rust.rustcTarget
-  )) crossPkgsFor;
-  rustStdFor = lib.mapAttrs (lib.const (
-    rustcTarget: fenix.targets.${rustcTarget}.stable.rust-std
-  )) rustcTargetFor;
-
-  toolchain =
-    with fenix;
-    combine (
-      [
-        stable.cargo
-        stable.rustc
-      ]
-      ++ lib.attrValues rustStdFor
-    );
-
-  crossPlatformFor = lib.mapAttrs (lib.const (
-    pkgs:
-    pkgs.makeRustPlatform (
-      lib.genAttrs [
-        "cargo"
-        "rustc"
-      ] (lib.const toolchain)
-    )
-  )) crossPkgsFor;
 in
 { arch }:
-nixpkgs-tracker-bot.override {
-  rustPlatform = crossPlatformFor.${arch};
-  inherit (crossPkgsFor.${arch}) openssl;
-  optimizeSize = true;
-}
+let
+  crossPkgs = crossPkgsFor.${arch};
+in
+(crossPkgs.callPackage ./package.nix { optimizeSize = true; }).overrideAttrs (old: {
+  passthru = old.passthru or { } // {
+    inherit crossPkgs;
+  };
+})
